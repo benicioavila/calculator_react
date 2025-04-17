e9c2fd18e31b347408b63da51a96a1a17c25757e
Benicio Brito <benicio.brito@sepog.fortaleza.ce.gov.br>
2025-04-17 14:27:49 -0300

    initial design

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Calculator = () => {
+    return (
+        <div className="Calculato" >            
+            <div className="complete-operation"> 3 * 3</div>
+            <div className="display">6</div>
+            <div className="button"></div>
+            <button>=</button>
+
+        </div>
+    );
+};
+
+export default Calculator;
