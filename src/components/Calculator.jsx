b74ef9fa48f10eb40281d09fef5fba62ac8fbe5e
Benicio Brito <benicio.brito@sepog.fortaleza.ce.gov.br>
2025-04-17 14:54:45 -0300

    creating css

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
+import './Calculator.css';
+
 
 const Calculator = () => {
+
+    const keypadNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    const operations = ["+", "-", "*", "/"]
+
     return (
-        <div className="Calculato" >            
+        <div className="calculator" >            
             <div className="complete-operation"> 3 * 3</div>
             <div className="display">6</div>
-            <div className="button"></div>
-            <button>=</button>
+            <div className="buttons">
+                <button>AC</button>
+                {keypadNumbers.map((number) => (
+                    <button key={number}>{number}</button>
+                ))}
+                {operations.map((operation) => (
+                    <button key={operation}>{operation}</button>
+                ))}
+                <button>=</button>
+            </div>
+            
 
         </div>
     );
